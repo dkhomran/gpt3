b59efb9c0f63bf6855cd2048027746c5d2cb682c
Dekhil Omran <dekhil22omran@gmail.com>
2023-01-25 23:23:04 +0100

    first commit

---
+++ src/data/data_features.js
@@ -0,0 +1,19 @@
+const Feature = [
+  {
+    id: "feature_0",
+    title: "Chatbots",
+    text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought.",
+  },
+  {
+    id: "feature_1",
+    title: "Knowledgebase",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments ",
+  },
+  {
+    id: "feature_2",
+    title: "Education",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b",
+  },
+];
+
+export default Feature;
